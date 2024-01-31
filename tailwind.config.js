3c641e5f921d8671e3ac68f511f894eee6ec81bf
Diego Perez <Diego.fernando.perez.luna@gmail.com>
2024-01-31 16:32:29 -0400

    Add: structure and styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        "primary": "#013CBE",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
